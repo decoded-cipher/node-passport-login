1a695a567383bfb0713dcfe2f6c149b972433081
Arjun Krishna <arjun12345krishna@gmail.com>
2020-03-27 03:08:43 +0530

    My commit:

--- app.js
+++ app.js
@@ -1,11 +1,53 @@
 const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
+const mongoose = require('mongoose');
+const passport = require('passport');
+const flash = require('connect-flash');
+const session = require('express-session');
 
 const app = express();
+
+// Passport Config
+require('./config/passport')(passport);
+
+//DB Config
+const  db = require('./config/keys').MongoURI;
+
+//Connect to Mongo
+mongoose.connect(db, { useNewUrlParser: true })
+.then(() => console.log('MongoDB Connected...'))
+.catch(err => console.log(err));
 
 //EJS
 app.use(expressLayouts);
 app.set('view engine', 'ejs');
+
+//BodyParser
+app.use(express.urlencoded({ extended: false }));
+
+// Express session
+app.use(
+    session({
+      secret: 'secret',
+      resave: true,
+      saveUninitialized: true
+    })
+  );
+  
+  // Passport middleware
+  app.use(passport.initialize());
+  app.use(passport.session());
+  
+  // Connect flash
+  app.use(flash());
+  
+  // Global variables
+  app.use(function(req, res, next) {
+    res.locals.success_msg = req.flash('success_msg');
+    res.locals.error_msg = req.flash('error_msg');
+    res.locals.error = req.flash('error');
+    next();
+  });
 
 //Routes
 app.use('/', require('./routes/index'));
